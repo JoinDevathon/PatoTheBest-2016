2e8902cf1140012d5f9adbe8f2941cefb2418741
Ben (OutdatedVersion) <outdated@outdatedversion.com>
2016-11-05 08:39:07 -0500

    hm

--- src/main/java/org/devathon/contest2016/DevathonPlugin.java
+++ src/main/java/org/devathon/contest2016/DevathonPlugin.java
@@ -2,16 +2,20 @@ package org.devathon.contest2016;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class DevathonPlugin extends JavaPlugin {
+public class DevathonPlugin extends JavaPlugin
+{
 
     @Override
-    public void onEnable() {
+    public void onEnable()
+    {
         // put your enable code here
     }
 
     @Override
-    public void onDisable() {
+    public void onDisable()
+    {
         // put your disable code here
     }
+
 }
 
